3d462daf159b0f251f7b07c108c7a5c6c341bfc5
grossteddy <57408735+grossteddy@users.noreply.github.com>
2020-11-05 20:40:35 +0200

    Update power.c

--- power.c
+++ power.c
@@ -1,32 +1,45 @@
-#include <stdio.h>
-#define e 2.7182
+#define EXP 2.7182
 
     double Pow(double x , int y)
      {
-        double ans = x;
-
-        if(y == 0 && x != 0)
+        //reason why I use ans and p instead of y and x is because I remember that C goes to the adress of x and y
+        //thus changing it's content and corrupting future power calculations 
+        double ans = 1;
+        int p = y;
+        //saves time if x is 0, also would remove math error from Pow(0,>=0)
+        if(x == 0)
         {
-            ans = 0;
+            return 0;
         }
-        else
+        //for positive y
+        if(p > 0)
         {
-            for (int i = 0; i < y-1; ++i)
+            while(p != 0)
             {
-            ans = ans*x;
+                ans = ans*x;
+                p = p-1;
             }
+            return ans;
         }
-        if(y > 0)
+        //for negative y
+        if(p < 0)
+        {
+            while(p != 0)
+            {
+                ans = ans*x;
+                p = p+1;
+            }
+            return 1/ans;
+        }
+        //for 0 y
+        if(p == 0)
         {
             return ans;
         }
-        else
-        {
-            return 1/ans;
-        }
+        return 0;
     }
 
     double Exp(int x)
         {
-            return Pow(e,x);
+            return Pow(EXP,x);
         }
