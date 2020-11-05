6fbd1d812a5df3fea85cb81488c2d69d52f77379
grossteddy <57408735+grossteddy@users.noreply.github.com>
2020-11-05 20:38:24 +0200

    Update myMath.h
    2nd year reworks

--- myMath.h
+++ myMath.h
@@ -1,9 +1,9 @@
-#define e 2.7182
+#include <stdio.h>
 
-float add(float x , float y);
-float sub(float x , float y);
-double mul(double x , int y);
-double div(double x, int y);
+    float add(float x , float y);
+    float sub(float x , float y);
+    double mul(double x , int y);
+    double div(double x, int y);
 
-double Exp(int x);
-double Pow(double x , int y);
+    double Exp(int x);
+    double Pow(double x , int y);
