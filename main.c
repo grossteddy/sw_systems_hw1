8f80a8fdbd48f622313d676c8716cecfeb689f03
grossteddy <57408735+grossteddy@users.noreply.github.com>
2020-11-08 11:27:33 +0200

    Changed Exp to Exponent

--- main.c
+++ main.c
@@ -8,13 +8,13 @@ int main()
 
     float ans1,ans2,ans3;
 
-    ans1 = sub(add(Exp((int) num),Pow(num,3)),3);
+    ans1 = sub(add(Exponent((int) num),Power(num,3)),3);
     printf("The value of f(x)= e^x+x^3-3 at the point of %.4lf is: %.4lf \n", num, ans1);
 
-    ans2 = add(mul(num,3),mul(Pow(num,2),2));
+    ans2 = add(mul(num,3),mul(Power(num,2),2));
     printf("The value of f(x)= 3x+2x^2 at the point of %.4lf is: %.4lf \n", num, ans2);
 
-    ans3 = sub(div(mul(Pow(num,3),4),5),mul(num,2));
+    ans3 = sub(div(mul(Power(num,3),4),5),mul(num,2));
     printf("The value of f(x)= (4x^3)/5-2x at the point of %.4lf is: %.4lf \n", num, ans3);
 
     return 0;
